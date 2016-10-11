c8fc911550becd739b12b982e1d7033aa9bd0841
satub <satu.barnhill@gmail.com>
2016-10-11 17:50:14 -0400

    Done.

--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -4,6 +4,7 @@ class GithubService
 
   def initialize(access_hash = nil)
     @access_token = access_hash["access_token"] if access_hash
+    # binding.pry
   end
 
   def authenticate!(client_id, client_secret, code)
@@ -27,6 +28,7 @@ class GithubService
   end
 
   def create_repo(name)
+    # binding.pry
     Faraday.post "https://api.github.com/user/repos", {name: name}.to_json, {'Authorization' => "token #{self.access_token}", 'Accept' => 'application/json'}
   end
 
